30c78c4cdec5e6d2e7ec18a5437499c8f571c319
Lorena <lorena.giddens@outlook.com>
2018-08-07 18:27:37 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,4 @@
 class PigLatinizer
-  attr_reader :text
 
   def starts_with_vowel?(word)
     !!/^[AEIOUaeiou]/.match(word)
@@ -23,28 +22,5 @@ class PigLatinizer
       end
     end.join(" ")
   end
-
-  # def piglatinize(text)
-  #   vowels = /[aeiou]/
-  #   consonants = /[bcdfghjklmnpqrstvwxyz]/
-  #
-  #   @text_array = text.split(" ")
-  #   @text_array.map do |word|
-  #     if word[0].match(vowels)
-  #       word + "way"
-  #     else
-  #       begins_with_consonant = word.index(vowels)
-  #       move_letters(word, begins_with_consonant)
-  #     end
-  #   end.join(" ")
-  # end
-  #
-  # def move_letters(word, consonant)
-  #   word_array = word.split("")
-  #   consonant.times do
-  #     word_array.insert(word.length-1, word_array.delete_at(o))
-  #   end
-  #   word_array.join("") + "ay"
-  # end
 
 end
