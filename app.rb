ba0e755e42592b44a98bab2cf4d2faa26af29e40
Lorena <lorena.giddens@outlook.com>
2018-08-07 18:32:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @piglatinizer = PigLatinizer.new(params[:user_input])
+    @text = PigLatinizer.new(params[:user_phrase])
 
     erb :results
   end
